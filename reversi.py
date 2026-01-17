d9491f8a5a182e63046c822a322ae456ef8d32f7
Benjamin Moran <benmoran@protonmail.com>
2026-01-17 17:42:16 +0900

    * Add more piece graphics. * Add weighted randomization. * Pre-load textures. * Remove piece Shapes. * Use ENUM for Colors.

--- reversi.py
+++ reversi.py
@@ -1,7 +1,11 @@
+import enum
 import random
+
+from collections import deque
 
 import pyglet
 
+from pyglet.enums import CompareOp
 from pyglet.shapes import RoundedRectangle
 
 
@@ -19,21 +23,27 @@ def load_centered_texture(name):
     return texture
 
 
+class COLORS(enum.StrEnum):
+    WHITE = 'white'
+    BLACK = 'black'
+
+
 class Board:
 
     background_color = (200, 200, 200)
     cell_color = (50, 50, 50)
     cell_highlight_color = (80, 80, 80)
-    white = (255, 255, 255)
-    black = (0, 0, 0)
 
-    white_faces = ('white1.png', 'white2.png', 'white3.png', 'white4.png', 'white5.png',
-                  'white6.png', 'white7.png', 'white8.png', 'white9.png', 'white10.png')
+    white_faces = [f"white{i}.png" for i in range(1, 20 + 1)]
+    black_faces = [f"black{i}.png" for i in range(1, 16 + 1)]
 
     def __init__(self, window, batch, num_cells=8):
         self.window = window
         self.batch = batch
         self.num_cells = num_cells
+
+        self.white_textures = deque([load_centered_texture(face) for face in self.white_faces])
+        self.black_textures = deque([load_centered_texture(face) for face in self.black_faces])
 
         # Calculated on resize:
         self.x = 0
@@ -43,69 +53,72 @@ class Board:
         self.sectorsize = 1
 
         self.bg_group = pyglet.graphics.Group(order=0)
-        self.fg_group = pyglet.graphics.Group(order=1)
-        self.pc_group = pyglet.graphics.Group(order=2)
-        self.hl_group = pyglet.graphics.Group(order=3)
+        self.bg_group.set_depth_test(CompareOp.LESS)
+        self.pc_group = pyglet.graphics.Group(order=1)
 
         # Shapes:
-        self.background = None
-        self.cell_shapes = []
-        self.piece_shapes = []
+        self.board_shapes = []
         self.face_sprites = []
-        self.cell_map = {}
-        self.piece_map = {}
-        self.face_map = {}
+        self.shape_map = {}
+        self.texture_map = {}
         self.selected_cell = None
 
-        self.current_color = self.white
+        self.current_color = COLORS.WHITE
         self.color_map = {}
 
         self.create_board()
 
+    def _get_random_face(self, color):
+        """Get a random face from the top of the deque, then rotate."""
+        texture_list_name = f"{color}_textures"
+        textures = getattr(self, texture_list_name)
+        num_items = len(textures) - 1
+        index = random.randint(num_items // 2, num_items)   # top half only
+        choice = textures[index]
+        textures.remove(choice)
+        textures.insert(0, choice)
+        textures.rotate(1)
+        setattr(self, texture_list_name, textures)
+        return choice
+
     def create_board(self):
         # Calculate the background
-        r = 8
-        self.background = RoundedRectangle(self.x, self.y, self.size, self.size, r, color=self.background_color, group=self.bg_group, batch=self.batch)
+        background = RoundedRectangle(self.x, self.y, self.size, self.size, radius=8,
+                                      color=self.background_color, group=self.bg_group, batch=self.batch)
+        background.z = -1.0
+        self.board_shapes.append(background)
 
         # Calculate the Cells:
         gapsize = self.size * 0.01
         cellsize = (self.size - gapsize * (self.num_cells + 1)) / self.num_cells
         startx = self.x + gapsize
         starty = self.y + gapsize
-        r = max(2.0, gapsize)
+        radius = max(2.0, gapsize)
 
         for row in range(self.num_cells):
             y = starty + row * (cellsize + gapsize)
             for column in range(self.num_cells):
                 x = startx + column * (cellsize + gapsize)
-                cell = RoundedRectangle(x, y, cellsize, cellsize, r,
-                                        color=self.cell_color, group=self.fg_group, batch=self.batch)
-                self.cell_map[(column, row)] = cell
-                self.cell_shapes.append(cell)
+                cell = RoundedRectangle(x, y, cellsize, cellsize, radius,
+                                        color=self.cell_color, group=self.bg_group, batch=self.batch)
+
+                self.shape_map[(column, row)] = cell
+                self.board_shapes.append(cell)
 
         # Calculate the Pieces:
         sectorsize = self.sectorsize
-        radius = sectorsize / 3
-        for column, row in self.piece_map.keys():
+        for (column, row), texture in self.texture_map.items():
             x = self.x + (column * sectorsize + sectorsize / 2)
             y = self.y + (row * sectorsize + sectorsize / 2)
-            color = self.color_map[(column, row)]
-            circle = pyglet.shapes.Circle(x, y, radius, color=color, group=self.pc_group, batch=self.batch)
-            self.piece_map[(column, row)] = circle
-
-            texture = load_centered_texture(name=random.choice(self.white_faces))
             face = pyglet.sprite.Sprite(texture, x, y, group=self.pc_group, batch=self.batch)
             face.scale = sectorsize / texture.height * 0.8
-            self.face_map[(column, row)] = face
+            self.face_sprites.append(face)
 
     def delete_board(self):
-        for item in self.cell_shapes + self.piece_shapes + self.face_sprites:
+        for item in self.board_shapes + self.face_sprites:
             item.delete()
-        self.cell_shapes.clear()
-        self.piece_shapes.clear()
+        self.board_shapes.clear()
         self.face_sprites.clear()
-        self.background.delete()
-        self.background = None
         self.selected_cell = None
 
     def _in_bounds(self, x, y) -> bool:
@@ -119,7 +132,7 @@ class Board:
     def _handle_mouse_motion(self, x, y):
         if self._in_bounds(x, y):
             column, row = self._get_sector(x, y)
-            cell = self.cell_map.get((column, row))
+            cell = self.shape_map.get((column, row))
 
             if self.selected_cell:
                 self.selected_cell.color = self.cell_color
@@ -156,22 +169,26 @@ class Board:
             return
 
         column, row = self._get_sector(x, y)
+
+        if color := self.color_map.get((column, row)):
+            # TODO: play a sound effect
+            print(f"Already color: ", color)
+            return
+
         sectorsize = self.sectorsize
-        radius = sectorsize / 3
         x = self.x + (column * sectorsize + sectorsize / 2)
         y = self.y + (row * sectorsize + sectorsize / 2)
-        circle = pyglet.shapes.Circle(x, y, radius, color=self.current_color, group=self.pc_group, batch=self.batch)
 
-        texture = load_centered_texture(name=random.choice(self.white_faces))
+        texture = self._get_random_face(color=self.current_color)
         face = pyglet.sprite.Sprite(texture, x, y, group=self.pc_group, batch = self.batch)
         face.scale = self.sectorsize / texture.height * 0.8
+        self.face_sprites.append(face)
 
-        self.face_map[(column, row)] = face
-        self.piece_map[(column, row)] = circle
+        self.texture_map[(column, row)] = texture
         self.color_map[(column, row)] = self.current_color
 
         # TODO: handle player changeover, move checking, etc.
-        self.current_color = {self.white: self.black, self.black: self.white}[self.current_color]
+        self.current_color = {COLORS.WHITE: COLORS.BLACK, COLORS.BLACK: COLORS.WHITE}[self.current_color]
 
 if __name__ == "__main__":
     board = Board(window=window, batch=batch)
